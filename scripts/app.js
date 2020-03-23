e5a5c47e6a23cd4a870afa8a0e9abcf74751f5e1
Cliff Conolly <cliffconolly@protonmail.com>
2020-03-23 18:55:37 +0000

    check to see if cityDets and weather come in via console

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,13 @@ const cityForm = document.querySelector('form');
 
 const updateCity = async (city) => {
 
-  console.log(city);
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return {
+    cityDets: cityDets,
+    weather: weather
+  };
 
 };
 
@@ -17,7 +23,9 @@ cityForm.addEventListener('submit', e => {
   cityForm.reset();
 
   // update the ui with new city
-  updateCity(city);
+  updateCity(city)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
    
 });
 
